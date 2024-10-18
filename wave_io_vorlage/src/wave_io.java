1f097dd4261526122c7862e20a6f6d035149e5b1
jonas kernchen <joke2169@bht-berlin.de>
2024-10-18 10:48:23 +0200

    refactor

--- wave_io_vorlage/src/wave_io.java
+++ wave_io_vorlage/src/wave_io.java
@@ -1,31 +1,18 @@
 import java.io.IOException;
 
 public class wave_io {
-    public static void main(String[] args) {
-        int samples = 0;
-        int validBits = 0;
-        long sampleRate = 0;
-        long numFrames = 0;
-        int numChannels = 0;
-
-        String inFilename = null;
-        String outFilename = null;
-
-        if (args.length < 1) {
-            try {
-                throw new WavFileException("At least one filename specified  (" + args.length + ")");
-            } catch (WavFileException e1) {
-                e1.printStackTrace();
-            }
-        }
-
-        // Samples in dem Array readWavFile.sound
-
-        inFilename = args[0];
-
-        // Implementierung bei einem Eingabeparameter
-
-        WavFile readWavFile = null;
+    private static String[] args;
+    private static int samples = 0;
+    private static int validBits = 0;
+    private static long sampleRate = 0;
+    private static long numFrames = 0;
+    private static int numChannels = 0;
+    
+    private static String inFilename = null;
+    private static String outFilename = null;
+    private static WavFile readWavFile = null;
+    
+    private static void sample2a() {
         try {
             readWavFile = WavFile.read_wav(inFilename);
 
@@ -51,34 +38,54 @@ public class wave_io {
             // TODO Auto-generated catch block
             e1.printStackTrace();
         }
+    }
+    private static void sample2e() {
+        // 2e Downsampling
+        for (int i = 0; i < samples; i++) {
+            // ********* ToDo ***************
+        }
+    }
+    private static void sample3b() {
+        // 3b Bitreduzierung
+        int reduced_bits = 1;
+        for (int i = 0; i < samples; i++) {
+            // ********* ToDo ***************
+        }
+    }
+    private static void sample3e() {
+        // 3e Bitreduzierung Differenz
+        int reduced_bits = 1;
+        for (int i = 0; i < samples; i++) {
+            // ********* ToDo ***************
+        }
+    }
+    
+    public static void main(String[] argsp) {
+        args = argsp;
+        if (args.length < 1) {
+            try {
+                throw new WavFileException("At least one filename specified  (" + args.length + ")");
+            } catch (WavFileException e1) {
+                e1.printStackTrace();
+            }
+        }
+
+        // Samples in dem Array readWavFile.sound
+
+        inFilename = args[0];
+
+        // Implementierung bei einem Eingabeparameter
+
+        wave_io.sample2a();
 
         // Implementierung bei Ein-und Ausgabeparameter
 
         outFilename = args[1];
         try {
 
-            // 2e Downsampling
-            for (int i = 0; i < samples; i++) {
-
-                // ********* ToDo ***************
-
-            }
-
-            // 3b Bitreduzierung
-            int reduced_bits = 1;
-            for (int i = 0; i < samples; i++) {
-
-                // ********* ToDo ***************
-
-            }
-
-            // 3e Bitreduzierung Differenz
-            reduced_bits = 1;
-            for (int i = 0; i < samples; i++) {
-
-                // ********* ToDo ***************
-
-            }
+            sample2e();
+            sample3b();
+            sample3e();
 
             WavFile.write_wav(outFilename, numChannels, numFrames, validBits, sampleRate, readWavFile.sound);
         } catch (Exception e) {
