e79d2450d3e525bad2047741ef35823b220a4cbb
jonas kernchen <joke2169@bht-berlin.de>
2024-11-01 12:41:20 +0100

    image title

--- abgabe/imageTitle.js
+++ abgabe/imageTitle.js
@@ -4,5 +4,6 @@ for (let i = 0; i < images.length; i ++) {
     let p = document.createElement("p");
     let src = images[i].src;
     p.innerText = src.substring(src.lastIndexOf('/'));
+    p.style.position = "relative";
     images[i].before(p);
 }
