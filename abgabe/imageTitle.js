56c40e2853aca914dc0809a08bdf4255e4e9c7e7
phni9317 <phni9317@bht-berlin.de>
2024-11-01 13:31:47 +0100

    indey verschoben

--- abgabe/imageTitle.js
+++ abgabe/imageTitle.js
@@ -3,7 +3,7 @@ let images = document.getElementsByClassName("img1")
 for (let i = 0; i < images.length; i ++) {
     let p = document.createElement("p");
     let src = images[i].src;
-    p.innerText = src.substring(src.lastIndexOf('/'));
+    p.innerText = src.substring(src.lastIndexOf('/') + 1);
     p.style.position = "relative";
     images[i].before(p);
 }
