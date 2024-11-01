c3ba2ff9238e0ac48d6a430acbe271bdb110e38b
phni9317 <phni9317@bht-berlin.de>
2024-11-01 15:49:46 +0100

    name ändern, irgend eine aufgane

--- abgabe/imageTitle.js
+++ abgabe/imageTitle.js
@@ -3,7 +3,7 @@ let images = document.getElementsByClassName("img1")
 for (let i = 0; i < images.length; i ++) {
     let p = document.createElement("p");
     let src = images[i].src;
-    p.innerText = src.substring(src.lastIndexOf('/') + 1);
+    p.innerText = src.substring(src.lastIndexOf('/') + 1).replace("Gruppe4_", "");
     p.style.position = "relative";
     images[i].before(p);
 }
