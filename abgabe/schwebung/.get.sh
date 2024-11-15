f857aef73119c9b3014128dcc1ee822824c7def3
jonas kernchen <joke2169@bht-berlin.de>
2024-11-15 14:36:59 +0100

    s tri b d

--- abgabe/schwebung/.get.sh
+++ abgabe/schwebung/.get.sh
@@ -3,3 +3,4 @@ wget -q -O "s-tri-A.png" "https://cdn.discordapp.com/attachments/130695956866793
 wget -q -O "s-tri-B.png" "https://cdn.discordapp.com/attachments/1306959568667938888/1306972315614969886/image.png?ex=67389c52&is=67374ad2&hm=78c0485361e2e0a0eabbe14846c47bcbf8e4e4f2225f4d3c0aae553c8d76aa26&"
 wget -q -O "s-tri-A-m.png" "https://cdn.discordapp.com/attachments/1306959568667938888/1306974702727594024/image.png?ex=67389e8b&is=67374d0b&hm=cee44fefbaedc63fc0eb5f03352591a51567cb68ba7e184ac6201c396163d7c2&"
 wget -q -O "s-tri-B-m.png" "https://cdn.discordapp.com/attachments/1306959568667938888/1306974873037308015/image.png?ex=67389eb4&is=67374d34&hm=92a80ec4fb3c44129dbc2dcf98b172dee272ee6aa8e92038e4f0f862beb636ea&"
+wget -q -O "s-tri-B-d.png" "https://cdn.discordapp.com/attachments/1306959568667938888/1306975954232414220/image.png?ex=67389fb5&is=67374e35&hm=ab4f0efd0f41def876a1ae8fac6e1246afd9f9afd7dcdd4ef27282a6a454e41c&"
