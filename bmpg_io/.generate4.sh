820cdc61b2e564b609a8cb9c06889288643f5462
jonas kernchen <joke2169@bht-berlin.de>
2024-11-29 10:33:55 +0100

    send it

--- bmpg_io/.generate4.sh
+++ bmpg_io/.generate4.sh
@@ -23,22 +23,42 @@ source .gradlerun.sh
 # gradlerun Manmade_Gruppe4 brightness_print
 # gradlerun Manmade_Gruppe4 contrast_print
 
-gradlerun Nature_Gruppe4 add_brightness 20 "aufgabe4-images"
-gradlerun Nature_Gruppe4 add_brightness 40 "aufgabe4-images"
-gradlerun Nature_Gruppe4 add_brightness 60 "aufgabe4-images"
-gradlerun Nature_Gruppe4 add_brightness 80 "aufgabe4-images"
+# gradlerun Nature_Gruppe4 add_brightness 20 "aufgabe4-images"
+# gradlerun Nature_Gruppe4 add_brightness 40 "aufgabe4-images"
+# gradlerun Nature_Gruppe4 add_brightness 60 "aufgabe4-images"
+# gradlerun Nature_Gruppe4 add_brightness 80 "aufgabe4-images"
 
-gradlerun Manmade_Gruppe4 add_brightness 20 "aufgabe4-images"
-gradlerun Manmade_Gruppe4 add_brightness 40 "aufgabe4-images"
-gradlerun Manmade_Gruppe4 add_brightness 60 "aufgabe4-images"
-gradlerun Manmade_Gruppe4 add_brightness 80 "aufgabe4-images"
+# gradlerun Manmade_Gruppe4 add_brightness 20 "aufgabe4-images"
+# gradlerun Manmade_Gruppe4 add_brightness 40 "aufgabe4-images"
+# gradlerun Manmade_Gruppe4 add_brightness 60 "aufgabe4-images"
+# gradlerun Manmade_Gruppe4 add_brightness 80 "aufgabe4-images"
 
-gradlerun Nature_Gruppe4 multiply_contrast 0.2 "aufgabe4-images"
-gradlerun Nature_Gruppe4 multiply_contrast 0.4 "aufgabe4-images"
-gradlerun Nature_Gruppe4 multiply_contrast 0.8 "aufgabe4-images"
-gradlerun Nature_Gruppe4 multiply_contrast 1.0 "aufgabe4-images"
+# gradlerun Nature_Gruppe4 multiply_contrast 0.2 "aufgabe4-images"
+# gradlerun Nature_Gruppe4 multiply_contrast 0.4 "aufgabe4-images"
+# gradlerun Nature_Gruppe4 multiply_contrast 0.8 "aufgabe4-images"
+# gradlerun Nature_Gruppe4 multiply_contrast 1.0 "aufgabe4-images"
 
-gradlerun Manmade_Gruppe4 multiply_contrast 0.2 "aufgabe4-images"
-gradlerun Manmade_Gruppe4 multiply_contrast 0.4 "aufgabe4-images"
-gradlerun Manmade_Gruppe4 multiply_contrast 0.8 "aufgabe4-images"
-gradlerun Manmade_Gruppe4 multiply_contrast 1.0 "aufgabe4-images"
+# gradlerun Manmade_Gruppe4 multiply_contrast 0.2 "aufgabe4-images"
+# gradlerun Manmade_Gruppe4 multiply_contrast 0.4 "aufgabe4-images"
+# gradlerun Manmade_Gruppe4 multiply_contrast 0.8 "aufgabe4-images"
+# gradlerun Manmade_Gruppe4 multiply_contrast 1.0 "aufgabe4-images"
+
+gradlerun Nature_Gruppe4 multiply_contrast "-0.2" "aufgabe4-images"
+gradlerun Nature_Gruppe4 multiply_contrast "-0.4" "aufgabe4-images"
+gradlerun Nature_Gruppe4 multiply_contrast "-0.8" "aufgabe4-images"
+gradlerun Nature_Gruppe4 multiply_contrast "-1.0" "aufgabe4-images"
+
+gradlerun Manmade_Gruppe4 multiply_contrast "-0.2" "aufgabe4-images"
+gradlerun Manmade_Gruppe4 multiply_contrast "-0.4" "aufgabe4-images"
+gradlerun Manmade_Gruppe4 multiply_contrast "-0.8" "aufgabe4-images"
+gradlerun Manmade_Gruppe4 multiply_contrast "-1.0" "aufgabe4-images"
+
+gradlerun Nature_Gruppe4 multiply_contrast "1.5" "aufgabe4-images"
+gradlerun Nature_Gruppe4 multiply_contrast "2.5" "aufgabe4-images"
+gradlerun Nature_Gruppe4 multiply_contrast "5.0" "aufgabe4-images"
+gradlerun Nature_Gruppe4 multiply_contrast "10.0" "aufgabe4-images"
+
+gradlerun Manmade_Gruppe4 multiply_contrast "1.5" "aufgabe4-images"
+gradlerun Manmade_Gruppe4 multiply_contrast "2.5" "aufgabe4-images"
+gradlerun Manmade_Gruppe4 multiply_contrast "5.0" "aufgabe4-images"
+gradlerun Manmade_Gruppe4 multiply_contrast "10.0" "aufgabe4-images"
