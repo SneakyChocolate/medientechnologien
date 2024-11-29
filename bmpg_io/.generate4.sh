530222a6322b6dae9c00ce572f176540a8e29550
jonas kernchen <joke2169@bht-berlin.de>
2024-11-29 09:46:18 +0100

    shell variables

--- bmpg_io/.generate4.sh
+++ bmpg_io/.generate4.sh
@@ -1,27 +1,27 @@
 
 source .gradlerun.sh
 
-gradlerun Nature_Gruppe4 red_only "" "aufgabe4-images"
-gradlerun Nature_Gruppe4 green_only "" "aufgabe4-images"
-gradlerun Nature_Gruppe4 blue_only "" "aufgabe4-images"
-gradlerun Nature_Gruppe4 ycbcr "" "aufgabe4-images"
-gradlerun Nature_Gruppe4 ycbcr_y "" "aufgabe4-images"
-gradlerun Nature_Gruppe4 ycbcr_cr "" "aufgabe4-images"
-gradlerun Nature_Gruppe4 ycbcr_cb "" "aufgabe4-images"
+# gradlerun Nature_Gruppe4 red_only "" "aufgabe4-images"
+# gradlerun Nature_Gruppe4 green_only "" "aufgabe4-images"
+# gradlerun Nature_Gruppe4 blue_only "" "aufgabe4-images"
+# gradlerun Nature_Gruppe4 ycbcr "" "aufgabe4-images"
+# gradlerun Nature_Gruppe4 ycbcr_y "" "aufgabe4-images"
+# gradlerun Nature_Gruppe4 ycbcr_cr "" "aufgabe4-images"
+# gradlerun Nature_Gruppe4 ycbcr_cb "" "aufgabe4-images"
 
-gradlerun Manmade_Gruppe4 red_only "" "aufgabe4-images"
-gradlerun Manmade_Gruppe4 green_only "" "aufgabe4-images"
-gradlerun Manmade_Gruppe4 blue_only "" "aufgabe4-images"
-gradlerun Manmade_Gruppe4 ycbcr "" "aufgabe4-images"
-gradlerun Manmade_Gruppe4 ycbcr_y "" "aufgabe4-images"
-gradlerun Manmade_Gruppe4 ycbcr_cr "" "aufgabe4-images"
-gradlerun Manmade_Gruppe4 ycbcr_cb "" "aufgabe4-images"
+# gradlerun Manmade_Gruppe4 red_only "" "aufgabe4-images"
+# gradlerun Manmade_Gruppe4 green_only "" "aufgabe4-images"
+# gradlerun Manmade_Gruppe4 blue_only "" "aufgabe4-images"
+# gradlerun Manmade_Gruppe4 ycbcr "" "aufgabe4-images"
+# gradlerun Manmade_Gruppe4 ycbcr_y "" "aufgabe4-images"
+# gradlerun Manmade_Gruppe4 ycbcr_cr "" "aufgabe4-images"
+# gradlerun Manmade_Gruppe4 ycbcr_cb "" "aufgabe4-images"
 
-gradlerun Nature_Gruppe4 brightness_print
-gradlerun Nature_Gruppe4 contrast_print
+# gradlerun Nature_Gruppe4 brightness_print
+# gradlerun Nature_Gruppe4 contrast_print
 
-gradlerun Manmade_Gruppe4 brightness_print
-gradlerun Manmade_Gruppe4 contrast_print
+# gradlerun Manmade_Gruppe4 brightness_print
+# gradlerun Manmade_Gruppe4 contrast_print
 
 gradlerun Nature_Gruppe4 add_brightness 20 "aufgabe4-images"
 gradlerun Nature_Gruppe4 add_brightness 40 "aufgabe4-images"
