3a5fc8cd8c09f2c83d947673b98c59cde07dd3f0
jonas kernchen <joke2169@bht-berlin.de>
2024-12-20 13:31:21 +0100

    median kaputt >:(

--- bmpg_io/.generate6.sh
+++ bmpg_io/.generate6.sh
@@ -3,6 +3,8 @@ source .gradlerun.sh
 
 gradlerun Nature_Gruppe4 graustufen "" "aufgabe6-images" &&
 gradlerun Manmade_Gruppe4 graustufen "" "aufgabe6-images" &&
+gradlerun Nature_Gruppe4_kaputt graustufen "" "aufgabe6-images" &&
+gradlerun Manmade_Gruppe4_kaputt graustufen "" "aufgabe6-images" &&
 
 # gradlerun Nature_Gruppe4 blur "1" "aufgabe6-images"
 # gradlerun Nature_Gruppe4 blur "2" "aufgabe6-images"
@@ -24,24 +26,29 @@ gradlerun Manmade_Gruppe4 graustufen "" "aufgabe6-images" &&
 # gradlerun Manmade_Gruppe4 blur_diff "3" "aufgabe6-images"
 # gradlerun Manmade_Gruppe4 blur_diff "9" "aufgabe6-images"
 
-gradlerun Nature_Gruppe4 gradient_filter "" "aufgabe6-images" &&
-gradlerun Manmade_Gruppe4 gradient_filter "" "aufgabe6-images" &&
+# gradlerun Nature_Gruppe4 gradient_filter "" "aufgabe6-images" &&
+# gradlerun Manmade_Gruppe4 gradient_filter "" "aufgabe6-images" &&
 
-gradlerun Nature_Gruppe4 gradient_filter_diff "" "aufgabe6-images" &&
-gradlerun Manmade_Gruppe4 gradient_filter_diff "" "aufgabe6-images" &&
+# gradlerun Nature_Gruppe4 gradient_filter_diff "" "aufgabe6-images" &&
+# gradlerun Manmade_Gruppe4 gradient_filter_diff "" "aufgabe6-images" &&
 
-gradlerun Nature_Gruppe4 median "1" "aufgabe6-images" &&
-gradlerun Nature_Gruppe4 median "2" "aufgabe6-images" &&
-gradlerun Nature_Gruppe4 median "3" "aufgabe6-images" &&
-gradlerun Nature_Gruppe4 median "9" "aufgabe6-images" &&
+# gradlerun Nature_Gruppe4 median "1" "aufgabe6-images" &&
+# gradlerun Nature_Gruppe4 median "2" "aufgabe6-images" &&
+# gradlerun Nature_Gruppe4 median "3" "aufgabe6-images" &&
+# gradlerun Nature_Gruppe4 median "9" "aufgabe6-images" &&
 
-gradlerun Manmade_Gruppe4 median "1" "aufgabe6-images" &&
-gradlerun Manmade_Gruppe4 median "2" "aufgabe6-images" &&
-gradlerun Manmade_Gruppe4 median "3" "aufgabe6-images" &&
-gradlerun Manmade_Gruppe4 median "9" "aufgabe6-images" &&
+# gradlerun Manmade_Gruppe4 median "1" "aufgabe6-images" &&
+# gradlerun Manmade_Gruppe4 median "2" "aufgabe6-images" &&
+# gradlerun Manmade_Gruppe4 median "3" "aufgabe6-images" &&
+# gradlerun Manmade_Gruppe4 median "9" "aufgabe6-images" &&
 
-gradlerun Nature_Gruppe4_kaputt median "1" "aufgabe6-images" &&
-gradlerun Nature_Gruppe4_kaputt median "2" "aufgabe6-images" &&
-gradlerun Nature_Gruppe4_kaputt median "3" "aufgabe6-images" &&
-gradlerun Nature_Gruppe4_kaputt median "9" "aufgabe6-images"
+# gradlerun Nature_Gruppe4_kaputt median "1" "aufgabe6-images" &&
+# gradlerun Nature_Gruppe4_kaputt median "2" "aufgabe6-images" &&
+# gradlerun Nature_Gruppe4_kaputt median "3" "aufgabe6-images" &&
+# gradlerun Nature_Gruppe4_kaputt median "9" "aufgabe6-images" &&
+
+gradlerun Manmade_Gruppe4_kaputt median "1" "aufgabe6-images" &&
+gradlerun Manmade_Gruppe4_kaputt median "2" "aufgabe6-images" &&
+gradlerun Manmade_Gruppe4_kaputt median "3" "aufgabe6-images" &&
+gradlerun Manmade_Gruppe4_kaputt median "9" "aufgabe6-images"
 
